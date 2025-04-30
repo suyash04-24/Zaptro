480a3925e511ef131064f422da9de3a77d9e64b1
Suyash kakasaheb lagad <118686001+suyash04-24@users.noreply.github.com>
2025-04-30 15:09:39 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,16 +5,16 @@ const About = () => {
   return (
     <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-20">
       <div className="max-w-5xl mx-auto bg-white rounded-2xl shadow-lg p-8 space-y-8">
-        <h1 className="text-4xl font-bold  text-center">About Zaptro</h1>
+        <h1 className="text-4xl font-bold  text-center">About Zayrah</h1>
 
         <p className="text-gray-700 text-lg">
-          Welcome to <span className="font-semibold text-red-600">Zaptro</span>, your one-stop destination for the latest and greatest in electronics. From cutting-edge gadgets to must-have accessories, we’re here to power up your tech life with premium products and unbeatable service.
+          Welcome to <span className="font-semibold text-red-600">Zayrah</span>, your one-stop destination for the latest and greatest in electronics. From cutting-edge gadgets to must-have accessories, we’re here to power up your tech life with premium products and unbeatable service.
         </p>
 
         <div className="space-y-6">
           <h2 className="text-2xl font-semibold text-red-600">Our Mission</h2>
           <p className="text-gray-700 text-base">
-            At Zaptro, our mission is to make innovative technology accessible to everyone. We’re passionate about connecting people with the tools and tech they need to thrive in a digital world — all at competitive prices and delivered with speed and care.
+            At Zayrah, our mission is to make innovative technology accessible to everyone. We’re passionate about connecting people with the tools and tech they need to thrive in a digital world — all at competitive prices and delivered with speed and care.
           </p>
         </div>
 
